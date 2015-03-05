c28402e3de67be81c6db1a74fab0d33b42eef252
Thomas Osborn <trosborn@gmail.com>
2015-03-04 20:41:16 -0800

    first and only commit

---
+++ form_helper.rb
@@ -0,0 +1,106 @@
+require 'benchmark/ips'
+
+Benchmark.ips do |x|
+  x.report("Master: array.length == 1") {
+    record = [:document]
+
+    object = record.is_a?(Array) ? record.last : record
+    raise ArgumentError, "First argument in form cannot contain nil or be empty" unless object
+  }
+
+  x.report("PR: array.length == 1") {
+    record = [:document]
+
+    record_is_array = record.is_a?(Array)
+    object = record_is_array ? record.last : record
+
+    if record_is_array && record.any?(&:nil?)
+      raise ArgumentError, "First argument in form cannot contain nil or be empty" unless object
+    end
+  }
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.report("Master: array.length == 2") {
+    record = [:document, :comment]
+
+    object = record.is_a?(Array) ? record.last : record
+    raise ArgumentError, "First argument in form cannot contain nil or be empty" unless object
+  }
+
+  x.report("PR: array.length == 2") {
+    record = [:document, :comment]
+
+    record_is_array = record.is_a?(Array)
+    object = record_is_array ? record.last : record
+
+    if record_is_array && record.any?(&:nil?) || object.nil?
+      raise ArgumentError, "First argument in form cannot contain nil or be empty"
+    end
+  }
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.report("Master: array.length == 3") {
+    record = [:admin, :document, :comment]
+
+    object = record.is_a?(Array) ? record.last : record
+    raise ArgumentError, "First argument in form cannot contain nil or be empty" unless object
+  }
+
+  x.report("PR: array.length == 3") {
+    record = [:admin, :document, :comment]
+
+    record_is_array = record.is_a?(Array)
+    object = record_is_array ? record.last : record
+
+    if record_is_array && record.any?(&:nil?) || object.nil?
+      raise ArgumentError, "First argument in form cannot contain nil or be empty"
+    end
+  }
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.report("Master: array.length == 4") {
+    record = [:admin, :document, :comment, :edit]
+
+    object = record.is_a?(Array) ? record.last : record
+    raise ArgumentError, "First argument in form cannot contain nil or be empty" unless object
+  }
+
+  x.report("PR: array.length == 4") {
+    record = [:admin, :document, :comment, :edit]
+
+    record_is_array = record.is_a?(Array)
+    object = record_is_array ? record.last : record
+
+    if record_is_array && record.any?(&:nil?) || object.nil?
+      raise ArgumentError, "First argument in form cannot contain nil or be empty"
+    end
+  }
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.report("Master: array.length == 5") {
+    record = [:admin, :document, :comment, :commentable, :image]
+
+    object = record.is_a?(Array) ? record.last : record
+    raise ArgumentError, "First argument in form cannot contain nil or be empty" unless object
+  }
+
+  x.report("PR: array.length == 5") {
+    record = [:admin, :document, :comment, :commentable, :image]
+
+    record_is_array = record.is_a?(Array)
+    object = record_is_array ? record.last : record
+
+    if record_is_array && record.any?(&:nil?) || object.nil?
+      raise ArgumentError, "First argument in form cannot contain nil or be empty"
+    end
+  }
+  x.compare!
+end
